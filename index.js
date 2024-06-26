fe7261ab8fe04c5db8100e5abd38c7ac3245286c
Vitalis Ian <viterlis254@gmail.com>
2024-06-26 13:56:31 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 function saturdayFun(activity = "roller-skate") {
-    return `This Saturday, I want to ${activity}!`;
-  }
-  saturdayFun();
-  
-  function mondayWork(activity = "go to the office") {
-    return `This Monday, I will ${activity}.`;
-  }
-  mondayWork();
-  
-  function wrapAdjective(para1 = "*") {
-    return function (para2 = "special") {
-      return `You are ${para1}${para2}${para1}!`;
-    };
-  }
-  wrapAdjective();
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+function wrapAdjective(param1 = "*") {
+  return function (param2 = "special") {
+    return `You are ${param1}${param2}${param1}!`;
+  };
+}
+wrapAdjective();
